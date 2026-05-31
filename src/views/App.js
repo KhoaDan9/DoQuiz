25aace18505c3ebfffb2e19ff120fb75f200db98
KhoaDan9 <khoadangx2@gmail.com>
2026-05-31 09:27:38 +0700

    up navbar

---
+++ src/views/App.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header/Header";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
